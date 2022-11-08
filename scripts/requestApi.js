3ae5200294291ce5162059e2ef92e5d1a39a2436
Paola <pa_rosa@msn.com>
2022-11-08 12:41:36 -0300

    feat: toast

--- scripts/requestApi.js
+++ scripts/requestApi.js
@@ -2,10 +2,10 @@ import { toast } from "../pages/login/login.js"
 
 const baseUrl = "https://m2-api-adot-pet.herokuapp.com/"
 
-async function allPets(){
+async function allPets() {
     const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Njc2ODYzMDksImV4cCI6MTY2ODI5MTEwOSwic3ViIjoiYTI1YjAxZmEtZWFmNS00NDExLWFiZDktOTJkNTJjODQzZjg3In0.c9y8SCMeFiuIqms0U2a1IWruh0A6NPoqMqcHCo-4ubw"
-    const request = await fetch(`${baseUrl}pets`,{
-        headers:{
+    const request = await fetch(`${baseUrl}pets`, {
+        headers: {
             "Content-Type": "application/json",
             "Authorization": `Bearer ${token}`
         }
@@ -15,38 +15,39 @@ async function allPets(){
 }
 
 
-async function login(email,password) {
+
+async function login(email, password) {
     const data = {
         "email": email,
         "password": password
     }
-const options = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data)
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    }
+    const responseJSON = await fetch('https://m2-api-adot-pet.herokuapp.com/session/login', options)
+        .then((response) => response.json())
+        .then((response) => {
+            console.log(response)
+            if (!response.message) {
+                localStorage.setItem("@KenzieCompany", JSON.stringify(response.token))
+                window.location.replace("../homeUser/index.html")
+            } else {
+                toast(response)
+                console.log(response)
+                setTimeout(() => {
+                    window.location.reload()
+                }, 3000)
+            }
+        }
+        )
+    return responseJSON
 }
 
-const responseJSON = await fetch('https://m2-api-adot-pet.herokuapp.com/session/login', options)
-.then((response) => response.json())
-.then((response) => { console.log(response.message)
-    if (!response.message) {
-        localStorage.setItem("@KenzieCompany", JSON.stringify(response.token))
-        window.location.replace("../homeUser/index.html")
-    } else {
-        toast(response)
-        console.log(response)
-        setTimeout(() => {
-            window.location.reload()
-        }, 3000) 
-    }   
-}
-)
-return responseJSON
-}
-
-export{
+export {
     allPets,
     login
 }
