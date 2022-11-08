bf703cc798c9883c8a8b0f3ce668c86061e84b30
megaurso <bernadogui@hotmail.com>
2022-11-08 16:50:47 -0300

    feat: create profile render

--- pages/userPerfil/userPerfil.js
+++ pages/userPerfil/userPerfil.js
@@ -1,3 +1,62 @@
+import { myPets, renderMyProfile } from "../../scripts/requestApi.js";
 import { verifyPermissionAdmin } from "../../scripts/verifyPermission.js";
 
 verifyPermissionAdmin()
+
+function logout(){
+    const btn = document.querySelector("#removeLS")
+    btn.addEventListener("click",()=>{
+        localStorage.removeItem("@KenzieCompany")
+    })
+}logout()
+
+async function renderProfileInfo(){
+    const user = await renderMyProfile()
+    const sectionProfile = document.querySelector(".profile-section")
+    const divHeroBg = document.createElement("div")
+    const divProfileImg = document.createElement("div")
+    const imgBg = document.createElement("img")
+    const divContainer = document.createElement("div")
+    const divProfileContainer = document.createElement("div")
+    const nameUser = document.createElement("h2")
+    const email = document.createElement("p")
+    const divProfileBtns = document.createElement("div")
+    const btnUpdate = document.createElement("button")
+    const btnDelete = document.createElement("button")
+
+
+    divHeroBg.classList.add("hero-bg")
+    divProfileImg.classList.add("profile-image")
+    divContainer.classList.add("container")
+    divProfileContainer.classList.add("profile-container")
+    email.id = "email"
+    divProfileBtns.classList.add("profile-buttons")
+    btnUpdate.classList.add("btn","purple")
+    btnUpdate.id = "updateInfo"
+    btnDelete.classList.add("btn","alert")
+    btnDelete.id = "deleteAccount"
+
+
+    imgBg.src = user.avatar_url
+    nameUser.innerText = user.name
+    email.innerText = user.email
+    btnUpdate.innerText = "Atualizar informações"
+    btnDelete.innerText = "Deletar conta"
+
+
+    divProfileImg.append(imgBg)
+    divHeroBg.append(divProfileImg)
+    divProfileBtns.append(btnUpdate,btnDelete)
+    divProfileContainer.append(nameUser,email,divProfileBtns)
+    divContainer.append(divProfileContainer)
+    sectionProfile.append(divHeroBg,divContainer)
+
+}renderProfileInfo()
+
+async function renderMyPets(){
+    const pets = await myPets()
+    pets.forEach((element) => {
+        console.log(element)
+        
+    });
+}renderMyPets()
--- scripts/requestApi.js
+++ scripts/requestApi.js
@@ -1,9 +1,10 @@
 import { toastRegister } from "../pages/registro/registro.js"
+import { getLocalStorage } from "./getLS.js"
 
 const baseUrl = "https://m2-api-adot-pet.herokuapp.com/"
 
 async function allPets() {
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Njc2ODYzMDksImV4cCI6MTY2ODI5MTEwOSwic3ViIjoiYTI1YjAxZmEtZWFmNS00NDExLWFiZDktOTJkNTJjODQzZjg3In0.c9y8SCMeFiuIqms0U2a1IWruh0A6NPoqMqcHCo-4ubw"
+    const token = getLocalStorage()
     const request = await fetch(`${baseUrl}pets`, {
         headers: {
             "Content-Type": "application/json",
@@ -45,41 +46,67 @@ async function login(email, password) {
     return responseJSON
 }
 
-export {
-    allPets,
-    login
+async function register(name, email, password, avatar) {
+    const data = {
+        "name": name,
+        "email": email,
+        "password": password,
+        "avatar_url": avatar
+    }
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+    const responseJSON = await fetch('https://m2-api-adot-pet.herokuapp.com/users', options)
+        .then((response) => response.json())
+        .then((response) => {
+            console.log(response)
+            if (!response.message) {
+                toastRegister(response)
+                setTimeout(() => {
+                    window.location.replace("../login/login.html")
+                }, 3000)
+            } else {
+                toastRegister(response)
+                setTimeout(() => {
+                    window.location.reload()
+                }, 2500)
+            }
+        })
+    return responseJSON
 }
 
+async function renderMyProfile(){
+    const token = getLocalStorage()
+    const request = await fetch(`${baseUrl}users/profile`,{
+        headers:{
+            "Content-Type":"application/json",
+            "Authorization": `Bearer ${token}`            
+        }
+    })
+    const response = await request.json()
+    return response
+}
 
-export async function register(name, email, password, avatar) {
-        const data = {
-            "name": name,
-            "email": email,
-            "password": password,
-            "avatar_url": avatar
+async function myPets(){
+    const token = getLocalStorage()
+    const request = await fetch(`${baseUrl}pets/my_pets`,{
+        headers:{
+            "Content-Type":"application/json",
+            "Authorization": `Bearer ${token}`            
         }
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        }
-            const responseJSON = await fetch('https://m2-api-adot-pet.herokuapp.com/users', options)
-                .then((response) => response.json())
-                .then((response) => {
-                    console.log(response)
-                if (!response.message) {
-                    toastRegister(response)
-                        setTimeout(() => {
-                            window.location.replace("../login/login.html")
-                        }, 3000)
-                    } else {
-                        toastRegister(response)
-                        setTimeout(() => {
-                            window.location.reload()
-                        }, 2500)
-                    }
-               })      
-               return responseJSON   
-    }
+    })
+    const response = await request.json()
+    return response
+}
+
+export {
+    allPets,
+    login,
+    register,
+    renderMyProfile,
+    myPets
+}
